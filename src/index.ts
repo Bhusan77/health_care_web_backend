801354ba34abbc09c3e1b3e9fa34297aaf510cee
Bhusan Shrestha <bhusansht@gmail.com>
2026-02-03 21:42:26 +0545

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,22 @@ import bodyParser from 'body-parser';
 import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
 import authRoutes from "./routes/auth.route";
+import path from 'path';
+import morgan from 'morgan';
+import adminRoute from './routes/admin/user.route';
 
 const app: Application = express();
-
+const corOptions = {
+    origin:["http://localhost:3000", "http://localhost:3001"]
+}
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoute);
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ success: "true", message: "Welcome to the API" });
 });
