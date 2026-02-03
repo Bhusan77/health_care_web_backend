0569b7c56f28b0be2034783ae629d6ea90bfd7e4
Bhusan Shrestha <bhusansht@gmail.com>
2026-02-03 21:41:46 +0545

    auth.route updated

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizedMiddleware } from "../middleware/authorized.middleware";
+import { uploads } from "../middleware/upload.middleware";
 
 let authController = new AuthController();
 const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+router.put("/update", authorizedMiddleware, uploads.single("profile"), authController.updateUser)
+router.get("/users/:id", authController.getUserById)
 // add remaning routes like login, logout, etc.
 
 export default router;
